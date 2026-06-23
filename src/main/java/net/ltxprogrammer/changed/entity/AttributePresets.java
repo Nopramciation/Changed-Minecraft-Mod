dbc872a74a8d80dc86f206f5078bb6c8194b2e33
Nopramciation <1103512913@qq.com>
2026-06-23 18:39:03 +0800

    Update AttributePresets.java

--- src/main/java/net/ltxprogrammer/changed/entity/AttributePresets.java
+++ src/main/java/net/ltxprogrammer/changed/entity/AttributePresets.java
@@ -36,7 +36,7 @@ public abstract class AttributePresets {
 
     public static void sharkLike(AttributeMap map) {
         map.getInstance(Attributes.MOVEMENT_SPEED).setBaseValue(0.875);
-        map.getInstance(ForgeMod.SWIM_SPEED.get()).setBaseValue(1.17);
+        map.getInstance(ForgeMod.SWIM_SPEED.get()).setBaseValue(1.13);
         map.getInstance(Attributes.MAX_HEALTH).setBaseValue(24.0);
     }
 
